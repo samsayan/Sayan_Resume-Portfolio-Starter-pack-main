952eb78ce35965abe282e1805492652c1ef6bcc2
Sayan Samaddar <samsayan589@gmail.com>
2021-02-03 22:45:53 +0530

    Revert "Update Testimonials.js"
    This reverts commit a0f8eb9d5875373ce7f88c8087e6ecec79546560.

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -20,7 +20,7 @@ const Testimonials = ({ data }) => {
         <div className="row">
           <div className="two columns header-col">
             <h1>
-              <span>BrainStorm</span>
+              <span>Client Testimonials</span>
             </h1>
           </div>
 
